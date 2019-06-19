3bbebc695f75ed562abe2e8528f7560c15dcda5c
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 21:03:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     #make array of anagram
     anagram_array = @anagram.split("")
     sorted_anagram_array = anagram_array.sort
-
+    puts "anagram is #{@anagram}... array is: #{array}"
     matched_array = array.map { |word|
       word_array = word.split("")
       sorted_word_array = word_array.sort
