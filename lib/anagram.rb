18a4fbe22c899c95002c8e30cd75fee841da3193
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 21:07:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
     anagram_array = @anagram.split("")
     puts "anagram is #{@anagram}... array is: #{array}"
     matched_array = []
-    matched_array = array.map { |word|
+    matched_array = array.filter { |word|
       word_array = word.split("")
 
       if word_array.sort == anagram_array.sort
